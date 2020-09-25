503d026f513981abe3bd6fc5bb7e540fff95fb84
lkj626 <hwi2337@naver.com>
2020-09-26 04:33:36 +0900

    re
    test

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,73 @@
 <script>
-	export let name;
+  let text="a";
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+.container{
+  font-family: 'Black Han Sans', sans-serif;
+}
+.container_2{
+  font-family: 'Do Hyeon', sans-serif;
+}
+.container_3{
+  font-family: 'Gugi', cursive;
+}
+.container_4{
+  font-family: 'Nanum Gothic', sans-serif;
+}
+.container_5{
+  font-family: 'Nanum Myeongjo', serif;
+}
+.container_6{
+  font-family: 'Nanum Pen Script', cursive;
+}
+.container_7{
+  font-family: 'Noto Sans KR', sans-serif;
+}
+.container_8{
+  font-family: 'Roboto', sans-serif;
+}
 </style>
+<input type="text" bind:value="{text}" />
+
+<h1></h1>
+<div class="container">
+  <section>
+    <h1>{text}</h1>
+  </section>
+</div>
+<div class="container_2">
+  <section>
+    <h1>{text}</h1>
+  </section>
+</div>
+<div class="container_3">
+  <section>
+    <h1>{text}</h1>
+  </section>
+</div>s
+<div class="container_4">
+  <section>
+    <h1>{text}</h1>
+  </section>
+</div>
+<div class="container_5">
+  <section>
+    <h1>{text}</h1>
+  </section>
+</div>
+<div class="container_6">
+  <section>
+    <h1>{text}</h1>
+  </section>
+</div>
+<div class="container_7">
+  <section>
+    <h1>{text}</h1>
+  </section>
+</div>  s
+<div class="container_8">
+  <section>
+    <h1>{text}</h1>
+  </section>
+</div>
